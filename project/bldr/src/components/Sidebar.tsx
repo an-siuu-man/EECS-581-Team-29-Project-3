898f816c74dc4b2d042c849b5a4e73551d945a93
an-siuu-man <ansuman_sharma@protonmail.com>
2025-11-29 02:09:26 -0600

    Update metadata and improve button component styles

--- project/bldr/src/components/Sidebar.tsx
+++ project/bldr/src/components/Sidebar.tsx
@@ -30,6 +30,7 @@ import {
   Edit2,
   Check,
   X,
+  User,
 } from "lucide-react";
 import toastStyle from "@/components/ui/toastStyle";
 import { set } from "date-fns";
@@ -288,7 +289,7 @@ export function Sidebar() {
                                     },
                                     body: JSON.stringify({
                                       scheduleName: newScheduleName.trim(),
-                                      semester: activeSemester || "Fall",
+                                      semester: activeSemester || "Fall 2025",
                                       year: 2025,
                                     }),
                                   }
@@ -460,28 +461,7 @@ export function Sidebar() {
 
         {/* Bottom Section */}
         <div className="flex flex-row w-full items-center justify-start gap-2">
-          <svg
-            viewBox="0 0 24 24"
-            height={30}
-            width={30}
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M18.14 21.62C17.26 21.88 16.22 22 15 22H8.99998C7.77998 22 6.73999 21.88 5.85999 21.62C6.07999 19.02 8.74998 16.97 12 16.97C15.25 16.97 17.92 19.02 18.14 21.62Z"
-              stroke="#fafafa"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>
-            <path
-              d="M15 2H9C4 2 2 4 2 9V15C2 18.78 3.14 20.85 5.86 21.62C6.08 19.02 8.75 16.97 12 16.97C15.25 16.97 17.92 19.02 18.14 21.62C20.86 20.85 22 18.78 22 15V9C22 4 20 2 15 2ZM12 14.17C10.02 14.17 8.42 12.56 8.42 10.58C8.42 8.60002 10.02 7 12 7C13.98 7 15.58 8.60002 15.58 10.58C15.58 12.56 13.98 14.17 12 14.17Z"
-              stroke="#fafafa"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>
-          </svg>
+          <User />
           <AnimatePresence>
             {open && (
               <motion.div
